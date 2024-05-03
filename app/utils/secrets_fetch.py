b2a18001c8fd39274b703d78fbdeda96a2d7ad68
MathiasSoender <mathias_sgaard@hotmail.dk>
2024-05-03 11:38:14 +0200

    removed secrets

--- app/utils/secrets_fetch.py
+++ app/utils/secrets_fetch.py
@@ -1,13 +1,16 @@
 # BAD PRACTICE
 # DO NOT STORE SECRETS AS PLAIN TEXT - TODO: use AWS Secretsmanager or .env
 def get_db_username() -> str:
-    return "mathiassgaard"
+    return ""
+
 
 def get_db_password() -> str:
-    return "QEacrUTv3iN8X8r1"
+    return ""
+
 
 def get_Matter_client_id() -> str:
-    return "dyzj7ep72inuyoin14nn4t100z"
+    return ""
+
 
 def get_Matter_client_secret() -> str:
-    return "dvar75zytt0ow6y534ntmf5mayk7yua56a8b9am3f1qcf6vpgwh8"
+    return ""
